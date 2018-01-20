4320bd2a3cf7372327f8bf7fa6516eef76a52822
r53127 <rnkn@163.com>
2018-01-20 20:16:29 +0800

    revised the ping_test.py

--- ping_test.py
+++ ping_test.py
@@ -1,7 +1,16 @@
-import sys
+import subprocess
 
+cmd="cmd.exe"
 begin=1
-end=100
+end=200
 while begin<end:
-    print(begin)
-    begin+=1
+
+    p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,
+                   stdin=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+    address="ping 192.168.31."+str(begin)+"\n"
+    p.stdin.write(address.encode())
+    p.stdin.close()
+    p.wait()
+    print("execution result: %s"%p.stdout.read().decode("gbk"))
+    begin=begin+1
